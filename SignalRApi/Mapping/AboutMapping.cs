54af5a2ab48d6cc63160961cf01b626e8ea12dea
Cemal Gokalp Yilmaz <gokalpyilmaz2401@gmail.com>
2024-11-08 21:12:31 +0300

    Mappings are created

--- SignalRApi/Mapping/AboutMapping.cs
+++ SignalRApi/Mapping/AboutMapping.cs
@@ -1,9 +1,17 @@
 ﻿using AutoMapper;
+using SignalRDto.AboutDto;
+using SignalREntites.Entites;
 
 namespace SignalRApi.Mapping
 {
     public class AboutMapping:Profile
     {
-
+        public AboutMapping()
+        {
+            CreateMap<About, ResultAboutDto>().ReverseMap();
+            CreateMap<About, CreateAboutDto>().ReverseMap();
+            CreateMap<About, UpdateAboutDto>().ReverseMap();
+            CreateMap<About, GetAboutDto>().ReverseMap();
+        }
     }
 }
---
+++ SignalRApi/Mapping/SocialMediaMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SignalRDto.SocialMediaDto;
+using SignalREntites.Entites;
+
+namespace SignalRApi.Mapping
+{
+    public class SocialMediaMapping:Profile
+    {
+        public SocialMediaMapping()
+        {
+            CreateMap<SocialMedia,GetSocialMediaDto>().ReverseMap();
+            CreateMap<SocialMedia,UpdateSocialMediaDto>().ReverseMap();
+            CreateMap<SocialMedia,CreateSocialMediaDto>().ReverseMap();
+            CreateMap<SocialMedia,ResultSocialMediaDto>().ReverseMap();
+        }
+    }
+}
