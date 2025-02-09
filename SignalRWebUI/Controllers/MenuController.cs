54ccbd81d39e5747f2b4f4f1911961ce1b09b333
Cemal Gokalp Yilmaz <gokalpyilmaz2401@gmail.com>
2025-02-09 13:53:47 +0300

    Menu işlemleri yapıldı

---
+++ SignalRWebUI/Controllers/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.DTOs.ProductDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MenuController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44383/api/Product");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultProductWithCategory>>(jsonData);
+            return View(values);
+
+        }
+    }
+}
